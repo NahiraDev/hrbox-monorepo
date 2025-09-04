f393139042b740a0722e6cec0827c6746a162c23
mohammadsafdari <hosseinzadeh-nahira@hrbox.com>
2025-09-04 14:02:54 +0330

    Eduvation and skills

--- modules/basic-info/features/employees/Education.tsx
+++ modules/basic-info/features/employees/Education.tsx
@@ -1,6 +1,73 @@
+import { Card } from '@heroui/react';
+import { education } from 'mock';
+import { AppButton } from 'core/components';
+import { Buildings, Calendar, Designtools, Location, Trash } from 'iconsax-react';
+
+import { BasicInfoLayout } from '../../features/common';
+
 const Education = () => {
   return (
-    <div>Education</div>
-  )
-}
-export default Education
+    <>
+      <BasicInfoLayout
+        content={
+          <div className="grid grid-cols-4 gap-3 w-full">
+            {education.map((user, index) => (
+              <Card key={index} className="py-2 px-3">
+                <div className="flex items-center justify-between border-b-2 border-gray-200 p-1.5">
+                  <div className="flex items-center gap-2">
+                    <Designtools />
+                    <span className='font-bold'>{user.job}</span>
+                  </div>
+                  <div>
+                    <AppButton
+                      props={{
+                        size: 'xs',
+                        radius: 'sm',
+                        variant: 'light',
+                        isIconOnly: true,
+                        onPress: () => console.log('ss'),
+                        content: <Trash className="text-secondary-1000 group-hover:text-white" />,
+                        className: 'hover:!bg-red-500 transition-all duration-200',
+                      }}
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-col gap-2 p-2">
+                  <div className="flex items-center justify-between p-1.5">
+                    <div className="flex gap-2 ">
+                      <Buildings />
+                      <span>Uni</span>
+                    </div>
+                    <div>
+                      <span>{user.company}</span>
+                    </div>
+                  </div>
+                  <div className="flex items-center justify-between p-1.5">
+                    <div className="flex gap-2">
+                      <Calendar />
+                      <span>Date</span>
+                    </div>
+                    <div>
+                      <span>{user.date}</span>
+                    </div>
+                  </div>
+                  <div className="flex items-center justify-between p-1.5">
+                    <div className="flex gap-2">
+                      <Location />
+                      <span>Location</span>
+                    </div>
+                    <div>
+                      <span>{user.location}</span>
+                    </div>
+                  </div>
+                </div>
+              </Card>
+            ))}
+          </div>
+        }
+      />
+    </>
+  );
+};
+
+export default Education;
