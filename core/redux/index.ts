48ffd5005bcbec88ff9ab77e086e59ea9ea8d58e
Nima Rahimloo <nimarahimloo1379@gmail.com>
2025-09-20 19:27:40 +0330

    efef

--- core/redux/index.ts
+++ core/redux/index.ts
@@ -2,3 +2,5 @@ export * from './store';
 export * from './rootReducer';
 export * from './reducers';
 export * from './hook';
+export { storageInstance } from '@core/redux/asyncLocalStorage';
+export { asyncLocalStorage } from '@core/redux/asyncLocalStorage';
