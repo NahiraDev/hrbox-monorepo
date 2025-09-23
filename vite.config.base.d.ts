19af35159925bc793361d9255c45be9bd8ff181b
Nima Rahimloo <nimarahimloo1379@gmail.com>
2025-09-23 22:39:46 +0330

    change project structore

---
+++ vite.config.base.d.ts
@@ -0,0 +1,5 @@
+import { type ConfigEnv, type UserConfig } from 'vite';
+export declare const baseConfig: (env: ConfigEnv) => UserConfig;
+declare const _default: import("vite").UserConfigFnObject;
+export default _default;
+//# sourceMappingURL=vite.config.base.d.ts.map
