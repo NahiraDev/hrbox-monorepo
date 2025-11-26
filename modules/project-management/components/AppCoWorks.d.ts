30f95d4c0d5cb7474f1d64b8e4089c8e70a10b5a
Nima Rahimloo <nimarahimloo1379@gmail.com>
2025-11-26 19:13:14 +0330

    first commit

---
+++ modules/project-management/components/AppCoWorks.d.ts
@@ -0,0 +1,2 @@
+export declare const AppCoWorks: () => import("react/jsx-runtime").JSX.Element;
+//# sourceMappingURL=AppCoWorks.d.ts.map
---
+++ modules/project-management/components/AppTaskList.d.ts
@@ -0,0 +1,2 @@
+export default function AppTasksList(): import("react/jsx-runtime").JSX.Element;
+//# sourceMappingURL=AppTaskList.d.ts.map
