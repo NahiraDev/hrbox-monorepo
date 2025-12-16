85c0e46570fa998267fedde5b3856d3f423917a9
aliahanin <aliahanin47@gmail.com>
2025-12-16 17:44:20 +0330

    initialProgressBar

--- modules/job-gradings/pages/dashboard/Dashboard.tsx
+++ modules/job-gradings/pages/dashboard/Dashboard.tsx
@@ -1,4 +1,5 @@
 const Dashboard = () => {
+    
   return (
     <>
       <div className="w-full   flex flex-row gap-x-4 h-full ">
@@ -10,7 +11,28 @@ const Dashboard = () => {
                 <p className="text-[#04070E] font-sans text-[24px] font-semibold text-capitalize  ">
                   Progress status of projects
                 </p>
-                <hr className="w-full border-[#05587A]"  />
+                <hr className="w-full border-[#05587A]" />
+              </div>
+            </div>
+            <div className="w-[70%] flex flex-col border-primary border rounded-xl px-3 py-4 bg-[#DCF0F9]  ">
+              <div className="w-full pb-2.5">
+                <p className="text-[#04070E] font-sans text-[24px]  font-semibold text-capitalize  ">
+                  The state of the organization
+                </p>
+              </div>
+              <hr className="w-full border-[#05587A]" />
+              <div className="flex flex-col gap-3 pt-4 w-full items-center h-full">
+                <div className="bg-[#FFFFFF] py-3 px-3 flex flex-col rounded-xl w-full ">
+                  <div className="flex w-full justify-between items-center">
+                    <p className="text-[#04070E] font-sans text-[20px]  font-semibold text-capitalize  ">
+                      Number of jobs graded
+                    </p>
+                    <span className="text-[32px] text-[#1E3363] font-bold font-sans">
+                      28
+                    </span>
+                  </div>
+                  <hr className="w-full border-[#B2B2B2]" />
+                </div>
               </div>
             </div>
           </div>
@@ -18,7 +40,7 @@ const Dashboard = () => {
             <div className="h-inherit w-50 bg-red-400"></div>
           </div>
         </div>
-        <div className="right bg-primary w-88"></div>
+        <div className="right  bg-primary w-88"></div>
       </div>
     </>
   );
