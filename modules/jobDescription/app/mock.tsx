f162cbce92c310f876635af8602dc276c254382f
Mohammad Hossein Anvari <jdoe@example.com>
2025-12-22 08:45:59 +0330

    update

---
+++ modules/jobDescription/app/mock.tsx
@@ -0,0 +1,50 @@
+export const jobDescriptions = [
+  {
+    id: 1,
+    title: "DNN Supervisor",
+    correctionDate: "2024/09/11",
+    positionCode: "123456",
+  },
+  {
+    id: 2,
+    title: "Frontend Developer",
+    correctionDate: "2024/08/01",
+    positionCode: "654321",
+  },
+  {
+    id: 3,
+    title: "DNN Supervisor",
+    correctionDate: "2024/09/11",
+    positionCode: "123456",
+  },
+  {
+    id: 4,
+    title: "Frontend Developer",
+    correctionDate: "2024/08/01",
+    positionCode: "654321",
+  },
+  {
+    id: 5,
+    title: "DNN Supervisor",
+    correctionDate: "2024/09/11",
+    positionCode: "123456",
+  },
+  {
+    id: 6,
+    title: "Frontend Developer",
+    correctionDate: "2024/08/01",
+    positionCode: "654321",
+  },
+  {
+    id: 7,
+    title: "DNN Supervisor",
+    correctionDate: "2024/09/11",
+    positionCode: "123456",
+  },
+  {
+    id: 8,
+    title: "Frontend Developer",
+    correctionDate: "2024/08/01",
+    positionCode: "654321",
+  },
+];
--- modules/jobDescription/pages/jobdescription/JobDescriptionCard.tsx
+++ modules/jobDescription/pages/jobdescription/JobDescriptionCard.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "@tanstack/react-router";
 import {
   ArchiveBook,
   Calendar,
@@ -5,47 +6,71 @@ import {
   Edit,
   Trash,
 } from "iconsax-reactjs";
+import { AppButton } from "@hrbox/uikit/components";
+import { useState } from "react";
+import clsx from "clsx";
 
-const JobDescriptionCard = () => {
+interface JobDescriptionCardProps {
+  title: string;
+  correctionDate: string;
+  positionCode: string;
+}
+
+const JobDescriptionCard = ({
+  title,
+  correctionDate,
+  positionCode,
+}: JobDescriptionCardProps) => {
+  const navigate = useNavigate();
   return (
     <>
-      <div className="w-[292px] px-4 py-3 flex flex-col gap-3 bg-white rounded-xl shadow-[0_1px_3px_0_rgba(8,14,28,0.3)]">
-        <div className="w-full flex justify-between items-center border-b-2 border-neutral-100">
+      <div
+        className={clsx(
+          "w-[292px] px-4 py-3 flex flex-col gap-3 bg-white rounded-xl shadow-[0_1px_3px_0_rgba(8,14,28,0.3)] cursor-pointer group",
+          "dark:shadow-none",
+          "hover:border hover:border-primary",
+          "active:border-primary! active:bg-[#D6F2FF]! dark:active:bg-[#04425C40]!" 
+        )}
+        onClick={()=>navigate({to:"/attendance/dashboard"})}
+      >
+        <div className="w-full flex justify-between items-center border-b-2 border-neutral-100 ">
           <div className="flex items-center gap-2">
-            <span>
+            <div>
               <ArchiveBook size={20} />
-            </span>
-            <p className="font-semibold text-[16PX]">DNN Supervisor</p>
+            </div>
+            <p className="font-semibold text-[16PX]">{title}</p>
           </div>
           <div className="flex gap-1 p-1">
-            <span>
-              <Edit size={17} />
-            </span>
-            <span>
-              <Trash size={17} />
-            </span>
+            <AppButton
+              className="p-1 group-hover:bg-[#FD8F02]! dark:group-hover:bg-[#FD8F02]! dark:bg-transparent!  rounded-sm group-hover:text-white "
+              content={<Edit size={17} />}
+            />
+            <AppButton
+              className="p-1 group-hover:bg-[#F23030]! dark:group-hover:bg-[#F23030]! dark:bg-transparent! rounded-sm group-hover:text-white "
+              content={<Trash size={17} />}
+            />
           </div>
         </div>
         <div className="w-full flex justify-between">
           <div className="gap-1 flex">
-            <span>
+            <div>
               <Calendar size={18} />
-            </span>
+            </div>
             <p className="text-sm">Correction date:</p>
           </div>
           <div>
-            <p className="text-sm">2024/09/11</p>
+            <p className="text-sm">{correctionDate}</p>
           </div>
         </div>
         <div className="w-full flex justify-between">
           <div className="flex gap-1">
-            <span>
+            <div>
               <DocumentText size={18} />
-            </span>
+            </div>
             <p className="text-sm font-normal">Poition Code:</p>
           </div>
           <div>
-            <p className="text-sm">123456</p>
+            <p className="text-sm">{positionCode}</p>
           </div>
         </div>
       </div>
