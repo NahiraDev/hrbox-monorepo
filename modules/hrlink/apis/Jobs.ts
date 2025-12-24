74593ba6bfaaa71d612528437dbee7d1c481a186
Nima Rahimloo <nimarahimloo1379@gmail.com>
2025-12-24 12:32:57 +0330

    Add Project Management TO Project

--- modules/hrlink/apis/Jobs.ts
+++ modules/hrlink/apis/Jobs.ts
@@ -1,53 +1,53 @@
-import { createMutation, createPaginatedQuery, createQuery } from "@hrbox/core/apis/createEndpoints";
+import { createMutation, createQuery } from "@hrbox/core/apis/createEndpoints";
 import { HRLinkApiEndpoints } from "@module/hrlink/app/endpoints";
-import { HRLinkApi } from '@module/hrlink/app/baseApi';
+import { HRLinkApi } from "@module/hrlink/app/baseApi";
 
 export const jobsApiEndpoints = HRLinkApi.injectEndpoints({
-  endpoints: (build:any) => ({
-    fetchJobOffers: createQuery<any, {Jobid: number}>(build, {
+  endpoints: (build: any) => ({
+    fetchJobOffers: createQuery<any>(build, {
       url: HRLinkApiEndpoints.job.getJobOfferDetail,
-      tags: ['Jobs'],
+      tags: ["Jobs"],
     }),
 
-    fetchJobOpportunities: createPaginatedQuery<any>(build, {
+    fetchJobOpportunities: createQuery<any>(build, {
       url: HRLinkApiEndpoints.job.opportunities,
-      tags: ['Jobs'],
+      tags: ["Jobs"],
     }),
 
-    fetchJobOfferDetail: createQuery<any, {Jobid: number}>(build, {
+    fetchJobOfferDetail: createQuery<any>(build, {
       url: HRLinkApiEndpoints.job.getJobOfferDetail,
-      tags: ['Jobs'],
+      tags: ["Jobs"],
     }),
 
     setTag: createMutation<any, any>(build, {
       url: HRLinkApiEndpoints.job.setTag,
-      method: 'POST',
-      tags: ['Jobs'],
+      method: "POST",
+      tags: ["Jobs"],
     }),
 
-    fetchJobOpportunitiesDetail: createQuery<any, {Jobid: number}>(build, {
+    fetchJobOpportunitiesDetail: createQuery<any>(build, {
       url: HRLinkApiEndpoints.job.useGetJobOpportunitiesDetail,
-      tags: ['Jobs'],
+      tags: ["Jobs"],
     }),
 
     fetchUserOrganization: createQuery<any>(build, {
       url: HRLinkApiEndpoints.job.getUserOrganization,
-      tags: ['Jobs'],
+      tags: ["Jobs"],
     }),
 
-    fetchAboutCompany: createQuery<any, {orgid: number}>(build, {
+    fetchAboutCompany: createQuery<any>(build, {
       url: HRLinkApiEndpoints.job.getAboutCompany,
-      tags: ['Jobs'],
+      tags: ["Jobs"],
     }),
 
-    fetchJobOfferListDetail: createPaginatedQuery<any>(build, {
+    fetchJobOfferListDetail: createQuery<any>(build, {
       url: HRLinkApiEndpoints.job.getJobOfferListDetail,
-      tags: ['Jobs'],
+      tags: ["Jobs"],
     }),
 
-    fetchListJobOffer: createPaginatedQuery<any>(build, {
+    fetchListJobOffer: createQuery<any>(build, {
       url: HRLinkApiEndpoints.job.getListJobOffer,
-      tags: ['Jobs'],
+      tags: ["Jobs"],
     }),
   }),
 });
--- modules/hrlink/pages/jobs/JobOpportunities.tsx
+++ modules/hrlink/pages/jobs/JobOpportunities.tsx
@@ -1,38 +1,58 @@
-import { Buildings2, Clock, DocumentForward, DollarCircle, Location, Status } from 'iconsax-reactjs';
-import {useFetchJobOpportunitiesQuery} from "@hrbox/modules/hrlink/apis";
-import {useNavigation} from "@hrbox/core/hooks/useNavigation";
-import {Button} from "@heroui/react";
-
+import {
+  Buildings2,
+  Clock,
+  DocumentForward,
+  DollarCircle,
+  Location,
+  Status,
+} from "iconsax-reactjs";
+import { useFetchJobOpportunitiesQuery } from "@hrbox/modules/hrlink/apis";
+import { useNavigation } from "@hrbox/core/hooks/useNavigation";
+import { Button } from "@heroui/react";
 
 const JobOpportunities = () => {
-  const {data:jobs} = useFetchJobOpportunitiesQuery();
-  const {push} = useNavigation();
+  const { data: jobs, isError, isSuccess } = useFetchJobOpportunitiesQuery();
+  const { push } = useNavigation();
 
   return (
     <div className="flex flex-col h-full justify-between">
       <div className="grid grid-cols-3 gap-3">
         {jobs &&
-            jobs.data.ViewList.map((oppertunitie: any) => (
-            <button key={oppertunitie.id} onClick={() => push({to:'/job/detail/' + oppertunitie.company})}>
+          jobs.data.ViewList.map((oppertunitie: any) => (
+            <button
+              key={oppertunitie.id}
+              onClick={() =>
+                push({ to: "/job/detail/" + oppertunitie.company })
+              }
+            >
               <div className="rounded-5 shadow-shadow-light-tight/1 px-3 py-4">
                 <div className="flex justify-between pb-1 border-b-1 border-neutral-100 dark:border-neutral-700">
                   <div className="flex items-center gap-2">
                     <Buildings2 className="text-secondary-400" size="22" />
-                    <span className="text-secondary-1000 font-semibold">{oppertunitie.Name}</span>
+                    <span className="text-secondary-1000 font-semibold">
+                      {oppertunitie.Name}
+                    </span>
                   </div>
                   <div className="flex gap-1">
                     <Button
                       className="!h-[22px] !w-[54px] !min-w-fit flex items-center gap-2 !px-1 !py-0.5 !rounded-2 bg-white"
                       variant="light"
                     >
-                      <span className="text-secondary-1000 text-[10px] font-normal">Easy Apply</span>
-                      <DocumentForward className="text-secondary-1000" size="12" />
+                      <span className="text-secondary-1000 text-[10px] font-normal">
+                        Easy Apply
+                      </span>
+                      <DocumentForward
+                        className="text-secondary-1000"
+                        size="12"
+                      />
                     </Button>
                   </div>
                 </div>
                 <div className="flex justify-between pt-2">
                   <div className="flex flex-col gap-1">
-                    <span className="text-secondary-800 font-bold text-sm leading-normal">{oppertunitie.title}</span>
+                    <span className="text-secondary-800 font-bold text-sm leading-normal">
+                      {oppertunitie.title}
+                    </span>
                     <div className="flex items-center gap-2">
                       <Location className="text-secondary-1000" size="16" />
                       <span className="text-secondary-1000 text-sm font-normal">
@@ -43,20 +63,30 @@ const JobOpportunities = () => {
                     </div>
                     <div className="flex items-center gap-2">
                       <Clock className="text-secondary-1000" size="16" />
-                      <span className="text-secondary-1000 text-sm font-normal">{oppertunitie.type}</span>
+                      <span className="text-secondary-1000 text-sm font-normal">
+                        {oppertunitie.type}
+                      </span>
                     </div>
                     <div className="flex items-center gap-2">
                       <DollarCircle className="text-secondary-1000" size="16" />
-                      <span className="text-secondary-1000 text-sm font-normal">{oppertunitie.salary}</span>
+                      <span className="text-secondary-1000 text-sm font-normal">
+                        {oppertunitie.salary}
+                      </span>
                     </div>
                     <div className="flex items-center gap-2">
                       <Status className="text-info" size="20" variant="Bold" />
-                      <span className="text-info-700 text-sm font-normal">{oppertunitie.status}</span>
+                      <span className="text-info-700 text-sm font-normal">
+                        {oppertunitie.status}
+                      </span>
                     </div>
                   </div>
                   <div className="flex flex-col gap-3">
                     <div className="flex justify-end">
-                      <img alt="" className="w-[84px] h-[84px] rounded-5" src={''} />
+                      <img
+                        alt=""
+                        className="w-[84px] h-[84px] rounded-5"
+                        src={""}
+                      />
                     </div>
                   </div>
                 </div>
