627ed415b7fad9ae9b6e809990d14f0a318e8679
Nima Rahimloo <nimarahimloo1379@gmail.com>
2025-12-30 15:46:47 +0330

    Fix Modal Footer

--- UIKit/components/AppModal.tsx
+++ UIKit/components/AppModal.tsx
@@ -366,7 +366,7 @@ const AppModalBase: React.FC<AppModalProps> & {
                 {children ?? modalComponent}
               </AppModalBody>
 
-              {!modalType && (
+              {modalType === "view" && (
                 <AppModalFooter
                   submitLabel={submitLabel}
                   cancelLabel={cancelLabel}
